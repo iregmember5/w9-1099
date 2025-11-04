f18491b5e057666e6dbeef7568fea9430c4e96eb
Saeed <faculty2.cust@gmail.com>
2025-11-04 18:26:39 +0500

    SAEED: W9-1099 componenet added

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import TaxFormsModule from './taxForms/TaxFormsModule'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
-      <div className="w-full max-w-4xl bg-white shadow-lg rounded-xl p-8">
+    <div className=" bg-gray-100 flex items-center justify-center p-6">
+      <div className="w-full bg-white shadow-lg rounded-xl p-8">
         <h1 className="text-2xl font-bold text-center mb-6 text-gray-800">
           W9 & 1099 Tax Forms Manager
         </h1>
